911008a22a9af5a5780630a3e2eba8cc92a0ce98
kielle <Kielle2001@gmail.com>
2022-08-02 19:43:58 +0200

    initial-0-updated-class-work

--- app/src/main/java/com/kiellenaidu/weather/MainActivity.java
+++ app/src/main/java/com/kiellenaidu/weather/MainActivity.java
@@ -1,6 +1,8 @@
 package com.kiellenaidu.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -10,13 +12,24 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "AccuWeatherURL";
-
+    FiveDayWeather FiveDayWeather;
+    TideFragment tideFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FiveDayWeather = new FiveDayWeather();
+        tideFragment = new TideFragment();
+
         URL accuWeatherURL = NetworkUtil.BuildURL();
         Log.i(TAG, " On Create: " + accuWeatherURL);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.weather_frame, FiveDayWeather);
+        transaction.replace(R.id.tide_frame,tideFragment);
+        transaction.commit();
+
     }
 }
--- app/src/main/java/com/kiellenaidu/weather/NetworkUtil.java
+++ app/src/main/java/com/kiellenaidu/weather/NetworkUtil.java
@@ -1,9 +1,15 @@
 package com.kiellenaidu.weather;
 
 import android.net.Uri;
+import android.util.Log;
+import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 
 public class NetworkUtil {
     private static final String BASE_URL = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/305605";
@@ -11,21 +17,56 @@ public class NetworkUtil {
     private static final String PARAM_API_KEY = "apikey";
     private static final String METRIC_PARAM = "metric";
     private static final String METRIC_VALUE = "true";
-
-   public static URL BuildURL() {
-       Uri uri = Uri.parse(BASE_URL).buildUpon().appendQueryParameter(PARAM_API_KEY, API_KEY ).appendQueryParameter(METRIC_PARAM, METRIC_VALUE).build();
-       URL url = null;
-
-       try{
-           url = new URL(uri.toString());
-       }catch (MalformedURLException e){
-           e.printStackTrace();
-       }
-       return url;
+    private static String TAG = " NETWORK_UTIL";
 
 
+    public static URL BuildURL() {
+        Uri uri = Uri.parse(BASE_URL).buildUpon().appendQueryParameter(PARAM_API_KEY, API_KEY).appendQueryParameter(METRIC_PARAM, METRIC_VALUE).build();
+        URL url = null;
 
-   } }
+        try {
+            url = new URL(uri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return url;
+
+
+    }
+
+    public static String getResponse(URL url) throws IOException {
+
+        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+        //Pull in existing java class
+        try {
+            //Gets input from the http GET request response
+            InputStream in = httpURLConnection.getInputStream();
+            Scanner scanner = new Scanner(in);
+
+            // Delimiter is used to find next line JSON
+            scanner.useDelimiter("//A");
+            //Checks if there is extra lines
+            boolean hasInput = scanner.hasNext();
+
+
+            if(hasInput)
+            {
+                //If there is return the next line
+                return  scanner.next();
+            } else {
+                //If there isnt extra lines return null
+                Log.i(TAG, "getResponse: " + scanner.next());
+                return null;
+            }
+
+        } finally {
+            httpURLConnection.disconnect();
+        }
+    }
+
+
+}
 
 
 
